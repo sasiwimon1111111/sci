d0e8c0b364fcbf806c56085647887698bbbf304a
Somkaid <blackcatozz19@gmail.com>
2023-09-21 12:20:53 +0700

    biot

--- biot/teacher.php
+++ biot/teacher.php
@@ -11,7 +11,80 @@
     <?php include('../components/navbar.php'); ?>
     <div class="container">
         <!-- content here -->
-        <h1>Hello World !!!</h1>
+        <div class="card bg-light px-sm-5">
+                <div class="row align-items-center g-4">
+                    <div class="col-sm-9">
+                        <div class="card-body">
+                            <!-- Title -->
+                            <h1 class="m-0 h4 card-title">บุคลากรสายวิชาการ คณะวิทยาศาสตร์และเทคโนโลยี</h1>
+                            <h6 class="lead1">มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <div class="card-group">
+            <div class="card">
+                <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_085939_edit.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">อ.จิตติมา กอหรั่งกูล</h5>
+                    <p class="card-text">สาขานวัตกรรมชีวผลิตภัณฑ์</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_094856_edit.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">ผศ.ณฐพงศ์ เมธินธรังสรรค์</h5>
+                    <p class="card-text">สาขานวัตกรรมชีวผลิตภัณฑ์</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_090120_edit.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">ผศ.ดร.ดวงเดือน วัฏฏานุรักษ์</h5>
+                    <p class="card-text">สาขานวัตกรรมชีวผลิตภัณฑ์</p>
+                </div>
+            </div>
+        </div>
+        <div class="card-group">
+            <div class="card">
+                <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_090016_edit.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">รศ.ดร.นฤมล ธนานันต์</h5>
+                    <p class="card-text"></p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_103825_edit.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">ผศ.ดร.พรรณวิภา แพงศรี</h5>
+                    <p class="card-text">สาขานวัตกรรมชีวผลิตภัณฑ์</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_094817_edit.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">ผศ.ดร.มัทนภรณ์ ใหม่คามิ</h5>
+                    <p class="card-text">สาขานวัตกรรมชีวผลิตภัณฑ์</p>
+                </div>
+            </div>
+        </div>
+        <div class="card-group">
+            <div class="card">
+                <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_095106_edit.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">ผศ.ดร.วัฒนา อัจฉริยะโพธา</h5>
+                    <p class="card-text">สาขานวัตกรรมชีวผลิตภัณฑ์</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_085610_edit.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">นางสาวพิมพ์นารา นิลฤทธิ์</h5>
+                    <p class="card-text">สาขานวัตกรรมชีวผลิตภัณฑ์</p>
+                </div>
+            </div>
+        </div>
+
 
 
         <!-- end content here -->
