7b2b7af8f2c93a6802d73d2040bfc63f67388c20
PeerapatAlex <137372420+PeerapatAlex@users.noreply.github.com>
2023-09-28 11:03:04 +0700

    imq

--- imq/overview.php
+++ imq/overview.php
@@ -1,23 +1,105 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-
-    <?php include('../components/style.php'); ?>
-</head>
-
-<body>
-
-    <?php include('../components/navbar.php'); ?>
-    <div class="container">
-        <!-- content here -->
-        <h1>Hello World !!!</h1>
-
-
-        <!-- end content here -->
+<!-- content here -->
+<h3 class="text-primary">ชื่อหลักสูตร : </h3>
+<table class="stack-on-tl">
+    <tbody>
+        <tr>
+            <td class="w20">ชื่อภาษาไทย</td>
+            <td>หลักสูตรวิทยาศาสตรบัณฑิต สาขาวิชามาตรวิทยาอุตสาหกรรมและ
+                ระบบคุณภาพ</td>
+        </tr>
+        <tr>
+            <td class="w20">ชื่อเต็มภาษาอังกฤษ</td>
+            <td>Bachelor of Science Program in Industrial Metrology and
+                Quality System</td>
+        </tr>
+    </tbody>
+</table>
+<h3><strong class="text-primary">ชื่อปริญญาและสาขาวิชา</strong></h3>
+<table class="stack-on-tl">
+    <tbody>
+        <tr>
+            <td class="w20">ชื่อเต็ม (ภาษาไทย)</td>
+            <td> วิทยาศาสตรบัณฑิต สาขาวิชามาตรวิทยาอุตสาหกรรมและระบบ
+                คุณภาพ</td>
+        </tr>
+        <tr>
+            <td class="w20">ชื่อเต็ม (ภาษาอังกฤษ)</td>
+            <td> Bachelor of Science Program in Industrial Metrology and
+                Quality System</td>
+        </tr>
+        <tr>
+            <td class="w20">ชื่อย่อ (ภาษาไทย)</td>
+            <td>วท.บ. (มาตรวิทยาอุตสาหกรรมและระบบคุณภาพ)</td>
+        </tr>
+        <tr>
+            <td class="w20">ชื่อย่อ (ภาษาอังกฤษ)</td>
+            <td>B.Sc. (Industrial Metrology and Quality System)</td>
+        </tr>
+    </tbody>
+</table>
+<h3 class="text-primary">จำนวนหน่วยกิต รูปแบบ และประเภทของหลักสูตร</h3>
+<table>
+    <tbody>
+        <tr>
+            <td><strong>จำนวนหน่วยกิตที่เรียน<br>
+                    ตลอดหลักสูตร</strong></td>
+            <td><strong>รูปแบบของหลักสูตร</strong></td>
+            <td><strong>ประเภทของหลักสูตร</strong></td>
+            <td><strong>สถานที่จัดการเรียนการสอน</strong></td>
+        </tr>
+        <tr>
+            <td>90 หน่วยกิต</td>
+            <td>หลักสูตรปริญญาตรี 2 ปี</td>
+            <td>
+                <ul>
+                    <li>เป็นหลักสูตรปริญญาตรีทางปฏิบัติการ</li>
+                </ul>
+            </td>
+            <td>
+                <ul>
+                    <li> อ.คลองหลวง จ.ปทุมธานี </li>
+                    <li> อ.เมือง จ.สระแก้ว </li>
+                </ul>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<div class="row">
+    <div class="col-6">
+        <h3><strong class="text-primary">ภาษาที่ใช้</strong></h3>
+        <p>หลักสูตรจัดการศึกษา เป็นภาษาไทย</p>
+        <h3 class="text-primary">แผนการรับนักศึกษา</h3>
+        <ul>
+            <li> รับนักศึกษาไทยและนักศึกษาต่างชาติที่สามารถใช้ภาษาไทยได้เป็นอย่างดี </li>
+        </ul>
     </div>
-    <?php include('../components/footer.php'); ?>
-    <?php include('../components/script.php'); ?>
-</body>
+    <div class="col-6">
+        <div class="video-container">
+            <iframe src="https://www.youtube.com/embed/rfqtLEtBp-Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+        </div>
+        <h5 align="center"><i class="glyphicon glyphicon-certificate"> วีดีทัศน์แนะนำหลักสูตร </i></h5>
+    </div>
+</div>
+<h3 class="text-primary">ตัวอย่างการประกอบอาชีพหลังจบการศึกษา</h3>
+<ul>
+    <li>งานราชการ, รัฐวิสาหกิจ</li>
+    <li>วิศวกรหรือผู้จัดการฝ่ายควบคุมคุณภาพ (Quality Control, QC Engineer or Quality
+        Control Manager) </li>
+    <li>วิศวกรผู้ดูแลระบบคุณภาพ (Quality System Engineer) </li>
+    <li>ผู้ประสานงานด้านระบบคุณภาพ (Quality System Coordinator)
+    </li>
+    <li>วิศวกรควบคุมคุณภาพของผลิตภัณฑ์ใหม่ (Production Quality Engineer/ Project
+        Quality Engineer)</li>
+    <li>วิศวกรควบคุมคุณภาพของสินค้าวัตถุดิบ (Supplier Quality Engineer) </li>
+    <li>วิศวกรควบคุมกระบวนการโดยใช้สถิติ (Statistical Process Control, SPC Engineer)</li>
+    <li>วิศวกรด้านงานประกันคุณภาพ (Quality Assurance, QA Engineer)</li>
+    <li> ผู้ดูแลระบบวัดคุมและเครื่องมือขั้นสูง (Instrument System Officer)</li>
+    <li>ผู้ให้ค าปรึกษาด้านเครื่องมือ (Product Instrument Specialist)</li>
+    <li>เจ้าหน้าที่สอบเทียบเครื่องมือวัดหรือนักวิชาการเกี่ยวกับงานรับรองมาตรฐานห้องปฏิบัติการ
+        ทั้งภาครัฐและเอกชน (Calibration Officer or ISO Officer)</li>
+    <li>ผู้ประกอบการด้านเครื่องมือ (Instrument Entrepreneur)</li>
+</ul>
 
-</html>
+
+
+<!-- end content here -->
--- imq/teacher.php
+++ imq/teacher.php
@@ -1,23 +1,23 @@
-<!doctype html>
-<html lang="en">
+<!-- content here -->
+<h1>บุคลากรสายวิชาการ</h1>
+<h2>คณะวิทยาศาสตร์และเทคโนโลยี</h2>
+<div class="card-group">
+    <div class="card">
+        <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_102059_edit.jpeg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">ผศ. วรายุทธ์ อัครพัฒนพงษ์</h5>
+            <p class="card-text">สาขามาตรวิทยาอุตสาหกรรมและระบบคุณภาพ</p>
 
-<head>
-
-    <?php include('../components/style.php'); ?>
-</head>
-
-<body>
-
-    <?php include('../components/navbar.php'); ?>
-    <div class="container">
-        <!-- content here -->
-        <h1>Hello World !!!</h1>
-
-
-        <!-- end content here -->
+        </div>
     </div>
-    <?php include('../components/footer.php'); ?>
-    <?php include('../components/script.php'); ?>
-</body>
+    <div class="card">
+        <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_095155_edit.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">ผศ.ดร.เยาวภา แสงพยับ</h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
 
-</html>
+        </div>
+    </div>
+</div>
+
+<!-- end content here -->
