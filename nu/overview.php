928308607a5b025fc20d1714286f62f72980745e
Somkaid <ballmoo159@gmail.com>
2023-09-28 10:23:20 +0700

    Nu

--- nu/overview.php
+++ nu/overview.php
@@ -1,23 +1,134 @@
-<!doctype html>
-<html lang="en">
 
-<head>
-
-    <?php include('../components/style.php'); ?>
-</head>
 
 <body>
 
-    <?php include('../components/navbar.php'); ?>
     <div class="container">
         <!-- content here -->
-        <h1>Hello World !!!</h1>
+        <div class="container"></div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="text-danger">
+                    <h3>ชื่อหลักสูตร:</h3>
+                </div>
+                <table class="stack-on-tl">
+                    <tbody>
+                        <tr>
+                            <td class="w20">ชื่อภาษาไทย</td>
+                            <td>สาขาโภชนาการและการกำหนดอาหาร</td><br>
+                        </tr>
+                        <tr>
+                            <td class="w20">ชื่อเต็มภาษาอังกฤษ</td>
+                            <td>Nutrition and Dietetics</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <h3><strong>ชื่อปริญญาและสาขาวิชา:</strong></h3>
+                <table class="stack-on-tl">
+                    <tbody>
+                        <tr>
+                            <td class="w20">ชื่อเต็ม (ภาษาไทย):</td>
+                            <td>สาขาวิชาโภชนาการและการกาหนด
+ อาหาร</td>
+                        </tr>
+                        <tr>
+                            <td class="w20">ชื่อเต็ม (ภาษาอังกฤษ):</td>
+                            <td>Bachelor of Science Program in Nutrition and Dietetics </td>
+                        </tr>
+                        <tr>
+                            <td class="w20">ชื่อย่อ (ภาษาไทย):</td>
+                            <td>วท.บ. (สาขาวิชาโภชนาการและการกาหนด
+ อาหา) </td>
+                        </tr>
+                        <tr>
+                            <td class="w20">ชื่อย่อ (ภาษาอังกฤษ):</td>
+                            <td> B.Sc. (Nutrition and Dietetics) </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
+            </div>
+            <div class="col-lg-6">
+                <?php include('../components/facebook.php'); ?>
+            </div><br>
+            <div class="row">
+                <div class="col-lg-8">
+                    <h3><strong>จำนวนหน่วยกิต รูปแบบ และประเภทของหลักสูตร</strong></h3>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td><strong>จำนวนหน่วยกิตที่เรียน
+                                        ตลอดหลักสูตร</strong></td>
+                                <td><strong>รูปแบบของหลักสูตร</strong></td>
+                                <td><strong>ประเภทของหลักสูตร</strong></td>
+                                <td><strong>สถานที่จัดการเรียนการสอน</strong></td>
+                            </tr>
+                            <tr>
+                                <td>140 หน่วยกิต</td>
+                                <td>หลักสูตรปริญญาตรี 4 ปี</td>
+                                <td>
+                                    <ul>
+                                        <li>หลักสูตรปริญญาตรีทางวิชาการ</li>
+                                    </ul>
+                                </td>
+                                <td>
+                                    <ul>
+                                        <li> อ.คลองหลวง จ.ปทุมธานี </li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8">
+                    <h3><strong>ภาษาที่ใช้</strong></h3>
+                    <p>หลักสูตรจัดการศึกษา เป็นภาษาไทย</p>
+                    <h3>แผนการรับนักศึกษา</h3>
+                    <ul>
+                        <li> จ.ปทุมธานี</li>
+                    </ul>
+                    <h3>รายละเอียดหลักสูตร </h3>
+                    <p>ระบบทวิภาค โดยหนึ่งปีการศึกษาแบ่งออกเป็น 2 ภาคการศึกษาปกติ แต่ละภาคการศึกษา ไม่น้อยกว่า 15 สัปดาห์ กรณีที่มีการจัดการศึกษาภาคฤดูร้อน ให้เป็นไปตามข้อบังคับมหาวิทยาลัย ราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์ จังหวัดปทุมธานี ว่าด้วยการจัดการศึกษาระดับ อนุปริญญาและปริญญาตรี พ.ศ. 2557 และแก้ไขเพิ่มเติม (ฉบับที่ 2) พ.ศ. 2561 และ (ฉบับที่ 3) พ.ศ. 2562 (ภาคผนวก ก) </p>
+                    <h3>ตัวอย่างการประกอบอาชีพหลังจบการศึกษา</h3>
+                    <table class="stack-on-mb">
+                        <tbody>
+                            <tr>
+                                <td>งานราชการ, รัฐวิสาหกิจ
+                                    <ul>
+                                        <li>นักโภชนาการ</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>อาชีพส่วนตัว
+                                    <ul>
+                                        <li>นักโภชนาการ</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                </div>
+                <div class="col-lg-4"></div>
+                <li>นอกจากนี้แล้ว บัณฑิตเมื่อสำเร็จการศึกษาหลักสูตร
+                    สามารถศึกษาต่อระดับปริญญาโท </li>
+                <p>สอบถามข้อมูลได้ที่ อาคาร 75 ปี คณะวิทยาศาสตร์และเทคโนโลยี
+                    มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์
+                    เลขที่ 1 หมู่ 20 ถนนพหลโยธิน กม. 48 ต.คลองหนึ่ง อ.คลองหลวง จ.ปทุมธานี 13180
+                    09-2265-8433
+                    sciencetech@vru.ac.th </p>
+            </div>
+        </div>
+
+
 
 
         <!-- end content here -->
     </div>
-    <?php include('../components/footer.php'); ?>
-    <?php include('../components/script.php'); ?>
+
 </body>
 
 </html>
--- nu/teacher.php
+++ nu/teacher.php
@@ -1,23 +1,99 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-
-    <?php include('../components/style.php'); ?>
-</head>
 
 <body>
 
-    <?php include('../components/navbar.php'); ?>
+
     <div class="container">
         <!-- content here -->
-        <h1>Hello World !!!</h1>
+        <h1></h1>
+        <section class="pt-4 pt-md-0">
+            <div class="container">
+                <!-- Title -->
+                <div class="row mb-2">
+                    <div class="col-12 text-center">
+                        <h3 class="mb-0">บุคลากรสายวิชาการ</h3>
+                        <p class="lead">คณะวิทยาศาสตร์และเทคโนโลยี</p>
+                    </div>
+                </div>
+        
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                    <img  src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_101323_edit.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">อ.จุฑาวรรณ นวลจันทร์คง</h5>
+                        <p class="card-text">สาขาโภชนาการและการกำหนดอาหาร</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_093321_edit.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">อ.นันทิภา แก้วลีลาวรรณ</h5>
+                        <p class="card-text">สาขาโภชนาการและการกำหนดอาหาร</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_101012_edit.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">ผศ.ปัทมาภรณ์ เจริญนนท์</h5>
+                        <p class="card-text">สาขาโภชนาการและการกำหนดอาหาร</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_101818_edit.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">ผศ.ศกุนตาล์ มานะกล้า</h5>
+                        <p class="card-text">สาขาโภชนาการและการกำหนดอาหาร</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_093612_edit.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">อ.ดร.สัจนา พัฒนาศักดิ์</h5>
+                        <p class="card-text">สาขาโภชนาการและการกำหนดอาหาร</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_100802_edit.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">อ.สุจา​ริณี​ สังข์​วรรณะ​</h5>
+                        <p class="card-text">สาขาโภชนาการและการกำหนดอาหาร</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_101452_edit.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">อ.ดร.ใยแพร ชาตรี</h5>
+                        <p class="card-text">สาขาโภชนาการและการกำหนดอาหาร</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_101715_edit.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">นางสาวทวีพร พันสวัสดิ์</h5>
+                        <p class="card-text">สาขาโภชนาการและการกำหนดอาหาร</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
 
 
         <!-- end content here -->
     </div>
-    <?php include('../components/footer.php'); ?>
-    <?php include('../components/script.php'); ?>
 </body>
 
 </html>
