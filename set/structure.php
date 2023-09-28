b1cf4046709011e2fa7af44f5eea412be3ac1ceb
sasiwimon1111111 <137372224+sasiwimon1111111@users.noreply.github.com>
2023-09-28 12:44:41 +0700

    set

--- set/structure.php
+++ set/structure.php
@@ -19,65 +19,65 @@
             <div>
                 <div style="font-size: 25px; color: rgb(81, 190, 233)">หลักสูตรประกอบไปด้วย</div>
 
-              
+
                 <table class="table">
-  <thead>
-    <tr>
-      <th scope="col"></th>
-      <th scope="col"></th>
-      <th scope="col">หมวดวิชา</th>
-      <th scope="col">จำนวนหน่วยกิต</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row"></th>
-      <td></td>
-      <td>หมวดวิชาศึกษาทั่วไป</td>
-      <td>ไม่น้อยกว่า 36 หน่วยกิต</td>
-    </tr>
-    <tr>
-      <th scope="row"></th>
-      <td></td>
-      <td>หมวดวิชาเฉพาะด้าน</td>
-      <td>ไม่น้อยกว่า 91 หน่วยกิต</td>
-    </tr>
-    <tr>
-      <th scope="row"></th>
-      <td></td>
-      <td>     1.กลุ่มวิชาแกน (พื้นฐานทางวิทยาศาสตร์และคณิตศาสตร์)</td>
-      <td>ไม่น้อยกว่า 26 หน่วยกิต</td>
-    </tr>
-    <tr>
-      <th scope="row"></th>
-      <td></td>
-      <td>2.กลุ่มวิชาเฉพาะด้าน</td>
-      <td>ไม่น้อยกว่า 15 หน่วยกิต</td>
-    </tr>
-    <tr>
-      <th scope="row"></th>
-      <td></td>
-      <td>-วิชาเฉพาะด้านบังคับ</td>
-      <td>ไม่น้อยกว่า 38 หน่วยกิต</td>
-    </tr>
-    <tr>
-      <th scope="row"></th>
-      <td></td>
-      <td>-วิชาเลืิอกเฉพาะด้าน</td>
-      <td>ไม่น้อยกว่า 12 หน่วยกิต</td>
-    </tr>
-    <tr>
-      <th scope="row"></th>
-      <td></td>
-      <td>3.หมวดวิชาเลือกเสรี</td>
-      <td>ไม่น้อยกว่า 6 หน่วยกิต</td>
-    </tr>
-    <tr>
-      <th scope="row"></th>
-      <td></td>
-      <td>หมวดวิชาประสบการณ์ภาคสนาม</td>
-      <td>ไม่น้อยกว่า 6 หน่วยกิต</td>
-    </tr>
+                    <thead>
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col"></th>
+                            <th scope="col">หมวดวิชา</th>
+                            <th scope="col">จำนวนหน่วยกิต</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row"></th>
+                            <td></td>
+                            <td>หมวดวิชาศึกษาทั่วไป</td>
+                            <td>ไม่น้อยกว่า 36 หน่วยกิต</td>
+                        </tr>
+                        <tr>
+                            <th scope="row"></th>
+                            <td></td>
+                            <td>หมวดวิชาเฉพาะด้าน</td>
+                            <td>ไม่น้อยกว่า 91 หน่วยกิต</td>
+                        </tr>
+                        <tr>
+                            <th scope="row"></th>
+                            <td></td>
+                            <td> 1.กลุ่มวิชาแกน (พื้นฐานทางวิทยาศาสตร์และคณิตศาสตร์)</td>
+                            <td>ไม่น้อยกว่า 26 หน่วยกิต</td>
+                        </tr>
+                        <tr>
+                            <th scope="row"></th>
+                            <td></td>
+                            <td>2.กลุ่มวิชาเฉพาะด้าน</td>
+                            <td>ไม่น้อยกว่า 15 หน่วยกิต</td>
+                        </tr>
+                        <tr>
+                            <th scope="row"></th>
+                            <td></td>
+                            <td>-วิชาเฉพาะด้านบังคับ</td>
+                            <td>ไม่น้อยกว่า 38 หน่วยกิต</td>
+                        </tr>
+                        <tr>
+                            <th scope="row"></th>
+                            <td></td>
+                            <td>-วิชาเลืิอกเฉพาะด้าน</td>
+                            <td>ไม่น้อยกว่า 12 หน่วยกิต</td>
+                        </tr>
+                        <tr>
+                            <th scope="row"></th>
+                            <td></td>
+                            <td>3.หมวดวิชาเลือกเสรี</td>
+                            <td>ไม่น้อยกว่า 6 หน่วยกิต</td>
+                        </tr>
+                        <tr>
+                            <th scope="row"></th>
+                            <td></td>
+                            <td>หมวดวิชาประสบการณ์ภาคสนาม</td>
+                            <td>ไม่น้อยกว่า 6 หน่วยกิต</td>
+                        </tr>
 
 
 
@@ -85,8 +85,8 @@
 
 
 
-  </tbody>
-</table>
+                    </tbody>
+                </table>
 
 
 
