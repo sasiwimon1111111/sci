543cb539d9f6f62e501807e541cbb45b5d742c62
Phanuwich48789 <opztv1240@gmail.com>
2023-09-28 11:07:45 +0700

    fb

--- fb/index.php
+++ fb/index.php
@@ -11,9 +11,33 @@
     <?php include('../components/navbar.php'); ?>
     <div class="container">
         <!-- content here -->
-        <h1>Hello World !!!</h1>
+        <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        <h1 class="display-8">ปริญญาตรี</h1>
+        <h1 class="display-4">สาขาวิชานวัตกรรมอาหารและเครื่องดื่มเพื่อสุขภาพ</h1>
+    </div>
+                <!-- Nav tabs -->
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">ภาพรวม</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">โครงสร้าง</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="messages-tab" data-bs-toggle="tab" data-bs-target="#messages" type="button" role="tab" aria-controls="messages" aria-selected="false">แผนการเรียน</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="settings-tab" data-bs-toggle="tab" data-bs-target="#settings" type="button" role="tab" aria-controls="settings" aria-selected="false">อาจารย์</button>
+  </li>
+</ul>
 
-
+<!-- Tab panes -->
+<div class="tab-content">
+  <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab" tabindex="0">ภาพรวม</div>
+  <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">โครงสร้าง</div>
+  <div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab" tabindex="0">แผนการเรียน</div>
+  <div class="tab-pane" id="settings" role="tabpanel" aria-labelledby="settings-tab" tabindex="0">อาจารย์</div>
+</div>
         <!-- end content here -->
     </div>
     <?php include('../components/footer.php'); ?>
