f7ef23e6edba9680b5720481d516b2726eb9dc1f
AzXI <102829587+AzXI01@users.noreply.github.com>
2023-09-28 10:36:34 +0700

    it

--- it/teacher.php
+++ it/teacher.php
@@ -11,8 +11,112 @@
     <?php include('../components/navbar.php'); ?>
     <div class="container">
         <!-- content here -->
-        <h1>Hello World !!!</h1>
-
+        <section class="pt-4 pt-md-0">
+            <div class="container">
+                <!-- Title -->
+                <div class="row mb-2">
+                    <div class="col-12 text-center">
+                    <h3>
+                        <strong>
+                            บุคลากรสายวิชาการ
+                        </strong>
+                    </h3>
+                        <p class="text-secondary">คณะวิทยาศาสตร์และเทคโนโลยี</p>
+                    </div>
+                </div>
+                <div class="card-deck">
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_092949_edit.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">ผศ.กมลมาศ  วงษ์ใหญ่</h5>
+        <p class="card-text">สาขาเทคโนโลยีสารสนเทศ</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_092705_edit.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">ผศ.ดร.กิตติศักดิ์  สิงห์สูงเนิน</h5>
+        <p class="card-text">สาขาเทคโนโลยีสารสนเทศ</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_092757_edit.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">อ.ชุมพล  จันทร์ฉลอง</h5>
+        <p class="card-text">สาขาเทคโนโลยีสารสนเทศ</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_093115_edit.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">ผศ.ทักษิณา  วิไลลักษณ์</h5>
+        <p class="card-text">สาขาเทคโนโลยีสารสนเทศ</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_105600_edit.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">อ.มัชฌกานต์    เผ่าสวัสดิ์</h5>
+        <p class="card-text">สาขาเทคโนโลยีสารสนเทศ</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_093246_edit.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">ผศ.อมีนา ฉายสุวรรณ</h5>
+        <p class="card-text">สาขาเทคโนโลยีสารสนเทศ</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_092921_edit.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">ผศ.อิงอร วงษ์ศรีรักษา</h5>
+        <p class="card-text">สาขาเทคโนโลยีสารสนเทศ</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_092824_edit.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">ผศ.อัจจิมา มั่นทน</h5>
+        <p class="card-text">สาขาเทคโนโลยีสารสนเทศ</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_093206_edit.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">อ.ไชย มีหนองหว้า</h5>
+        <p class="card-text">สาขาเทคโนโลยีสารสนเทศ</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="http://sci.vru.ac.th/assets/images/people/IMG_20230513_085715_edit.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">นายเนติวิทย์ วรรณโชติ</h5>
+        <p class="card-text">สาขาเทคโนโลยีสารสนเทศ</p>
+      </div>
+    </div>
+  </div>
+</div>
 
         <!-- end content here -->
     </div>
