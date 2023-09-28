c0cc063b4398c01f049d6d629e604e1a4d3bfcfb
phasakorn09 <nudangtop@gmail.com>
2023-09-28 10:19:38 +0700

    biot

--- biot/plan.php
+++ biot/plan.php
@@ -1,23 +1,111 @@
-<!doctype html>
-<html lang="en">
+<!-- content here -->
+<h2 class="text-primary">แผนการศึกษา</h2>
+<h4 class="text-primary">กลุ่มวิชาอาชีวอนามัยและความปลอดภัย ชั้นปีที่ 1</h4>
+<div class="col-12">
+    <table width="100%" class="table table-bordered">
+        <tbody>
+            <tr align="center">
+                <td colspan="4"><strong>ภาคการศึกษาที่ 1</strong></td>
+            </tr>
+            <tr align="center">
+                <td bgcolor="#D3D3D3">หมวดวิชา</td>
+                <td bgcolor="#D3D3D3"><strong>รหัสวิชา</strong></td>
+                <td bgcolor="#D3D3D3"><strong>ชื่อวิชา</strong></td>
+                <td bgcolor="#D3D3D3"><strong>หน่วยกิต</strong></td>
+            </tr>
+            <tr align="center">
+                <td valign="top">หมวดวิชาศึกษาทั่วไป</td>
+                <td valign="top">VGEXXX</td>
+                <td valign="top" align="left">&nbsp; หมวดวิชาศึกษาทั่วไป</td>
+                <td valign="top">7(X-X-X)</td>
+            </tr>
+            <tr align="center">
+                <td rowspan="3" valign="top">กลุ่มวิชาพื้นฐานวิทยาศาสตร์ฯ </td>
+                <td valign="top">SCH101</td>
+                <td valign="top" align="left">&nbsp; เคมีพื้นฐาน</td>
+                <td valign="top">3(2-2-5)</td>
+            </tr>
+            <tr align="center">
+                <td valign="top">SBT101</td>
+                <td valign="top" align="left">&nbsp; ชีววิทยาพื้นฐาน </td>
+                <td valign="top">3(2-2-5)</td>
+            </tr>
+            <tr align="center">
+                <td valign="top">SMS118</td>
+                <td valign="top" align="left">&nbsp; คณิตศาสตร์และสถิติพื้นฐาน</td>
+                <td valign="top">3(2-2-5)</td>
+            </tr>
+            <tr align="center">
+                <td rowspan="1" valign="top">หมวดวิชาเฉพาะ
+                    ด้านบังคับ
+                </td>
+                <td valign="top">SBT112</td>
+                <td valign="top" align="left">&nbsp; ความคิดสร้างสรรค์และนวัตกรรม
+                    ชีวผลิตภัณฑ์</td>
+                <td valign="top">3(2-2-5)</td>
+            </tr>
+            <tr align="right">
+                <td colspan="3" valign="top">รวมหน่วยกิต :&nbsp;</td>
+                <td align="center">19</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<div class="col-12">
+    <table width="100%" class="table table-bordered">
+        <tbody>
+            <tr align="center">
+                <td colspan="4"><strong>ภาคการศึกษาที่ 2</strong></td>
+            </tr>
+            <tr align="center">
+                <td bgcolor="#D3D3D3">หมวดวิชา</td>
+                <td bgcolor="#D3D3D3"><strong>รหัสวิชา</strong></td>
+                <td bgcolor="#D3D3D3"><strong>ชื่อวิชา</strong></td>
+                <td bgcolor="#D3D3D3"><strong>หน่วยกิต</strong></td>
+            </tr>
+            <tr align="center">
+                <td valign="top">หมวดวิชาศึกษาทั่วไป</td>
+                <td valign="top">VGEXXX</td>
+                <td valign="top" align="left">&nbsp; หมวดวิชาศึกษาทั่วไป</td>
+                <td valign="top">6(X-X-X)</td>
+            </tr>
+            <tr align="center">
+                <td rowspan="4" valign="top">กลุ่มวิชาพื้นฐานวิทยาศาสตร์ฯ </td>
+                <td valign="top">SBT201</td>
+                <td valign="top" align="left">&nbsp; พันธุศาสตร์</td>
+                <td valign="top">3(3-0-6)</td>
+            </tr>
+            <tr align="center">
+                <td valign="top">SBT202</td>
+                <td valign="top" align="left">&nbsp; ปฏิบัติการพันธุศาสตร์ </td>
+                <td valign="top">1(0-3-2)</td>
+            </tr>
+            <tr align="center">
+                <td valign="top">SBT203</td>
+                <td valign="top" align="left">&nbsp; จุลชีววิทยา</td>
+                <td valign="top">3(3-0-6)</td>
+            </tr>
+            <tr align="center">
+                <td valign="top">SBT204</td>
+                <td valign="top" align="left">&nbsp; ปฏิบัติการจุลชีววิทยา</td>
+                <td valign="top">1(0-3-2)</td>
+            </tr>
+            <tr align="center">
+                <td rowspan="1" valign="top">หมวดวิชาเฉพาะ
+                    ด้านบังคับ
+                </td>
+                <td valign="top">SBT113</td>
+                <td valign="top" align="left">&nbsp; เทคโนโลยีชีวภาพเพื่อการสร้าง
+                    นวัตกรรม</td>
+                <td valign="top">3(3-0-6)</td>
+            </tr>
+            <tr align="right">
+                <td colspan="3" valign="top">รวมหน่วยกิต :&nbsp;</td>
+                <td align="center">17ß</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 
-<head>
 
-    <?php include('../components/style.php'); ?>
-</head>
-
-<body>
-
-    <?php include('../components/navbar.php'); ?>
-    <div class="container">
-        <!-- content here -->
-        <h1>Hello World !!!</h1>
-
-
-        <!-- end content here -->
-    </div>
-    <?php include('../components/footer.php'); ?>
-    <?php include('../components/script.php'); ?>
-</body>
-
-</html>
+<!-- end content here -->
